919f3020c337400027023c34f3a73bb68034870e
Kassabji, Christophe <ChristopheKassabji@eaton.com>
2025-03-26 08:35:52 +0100

    Remove build artifacts and gradle wrapper from git tracking

--- HealthTracker/app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_healthtracker_HealthTrackerApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.healthtracker.HealthTrackerApp"
-)
-public final class _com_healthtracker_HealthTrackerApp {
-}
